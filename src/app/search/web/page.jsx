5db045fc99977360ccdbd4ce0b8ef6024eabd4fc
Kutbuddin khan <Kutbuddinkhan82@gmail.com>
2023-12-08 17:09:36 +0530

    handle possible errors

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,23 +1,38 @@
+import Link from "next/link";
 import React from "react";
 
-export default async function WebSearchPage({
-  searchParams
-}) {
+export default async function WebSearchPage({ searchParams }) {
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
 
+  if (!response.ok) {
+    throw new Error("Something went wrong");
+  }
+
   const data = await response.json();
-  console.log(data);
+  // console.log(data);
 
-  const result = data.items;
+  const results = data.items;
 
-  return <>
-    {result && 
-      result.map((result) => 
-        <h1>
-          {result.title}
-        </h1>)
-    }
-  </>;
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No Result found</h1>
+        <p className="text-lg">
+          Try searching for something else or go back to the Homepage.{" "}
+          <Link href="/" className="text-blue-500">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      {results.length > 0 &&
+        results.map((result, index) => <h1 key={index}>{result.title}</h1>)}
+    </>
+  );
 }
